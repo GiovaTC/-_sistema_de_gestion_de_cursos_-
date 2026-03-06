b1388fb0bf7872d06e7bfb4b80861b920895007a
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-06 10:07:38 -0500

    commit - Curso .

--- src/modelo/Curso.java
+++ src/modelo/Curso.java
@@ -1,4 +1,49 @@
 package modelo;
 
 public class Curso {
-}
+
+    private int idCurso;
+    private String nombre;
+    private String profesor;
+    private int creditos;
+
+    public Curso() {}
+
+    public Curso(String nombre, String profesor, int creditos) {
+        this.nombre = nombre;
+        this.profesor = profesor;
+        this.creditos = creditos;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+}   
