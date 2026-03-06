b58e7440b95905d707a1a313f61a265e1a9567b4
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-06 10:13:46 -0500

    commit - CursoDAO / .

--- src/dao/CursoDAO.java
+++ src/dao/CursoDAO.java
@@ -1,4 +1,33 @@
 package dao;
 
+import modelo.Curso;
+import util.ConexionDB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class CursoDAO {
-}
+
+    public void guardarCurso(Curso curso) {
+
+        String sql = """
+                INSERT INTO CURSOS
+                (NOMBRE, PROFESOR, CREDITOS)
+                VALUES (?, ?, ?)
+                """;
+        try (Connection conn = ConexionDB.obtenerConexion();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, curso.getNombre());
+            ps.setString(2, curso.getProfesor());
+            ps.setInt(3, curso.getCreditos());
+
+            ps.executeUpdate();
+
+            System.out.println("curso registrado exitosamente");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}   
